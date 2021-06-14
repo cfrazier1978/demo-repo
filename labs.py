0dca46853f8a87d571d66e87c4329dec3848055d
cfrazier1978 <66221343+cfrazier1978@users.noreply.github.com>
2021-06-14 18:14:43 -0400

    Update labs.py

--- labs.py
+++ labs.py
@@ -1,5 +1,5 @@
-from models import config1, config2
-from models.experiments import exp
+'from models import config1, config2
+'from models.experiments import exp
+from models.config import exp
 
-client_modules = [config1, config2]
-
+model_dir = 'models'
