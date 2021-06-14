71494475832747e1c8e775e4dfbd6fda41ff5d61
cfrazier1978 <66221343+cfrazier1978@users.noreply.github.com>
2021-06-14 18:14:58 -0400

    Update labs.py

--- labs.py
+++ labs.py
@@ -1,5 +1,5 @@
-'from models import config1, config2
-'from models.experiments import exp
+# from models import config1, config2
+# from models.experiments import exp
 from models.config import exp
 
 model_dir = 'models'
